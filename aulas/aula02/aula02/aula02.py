080c77b927a284daca7d7f3a8eec17bcee2aab9e
Letacio-Bezerra <letaciobezerra@gmail.com>
2025-05-20 13:35:12 -0300

    ss

---
+++ aulas/aula02/aula02/aula02.py
@@ -0,0 +1,25 @@
+# EXEMPLO 1: FOR
+for i in range(5):
+    print('Número: ',
+          i,
+          ', finalizou a interação')
+
+# EXEMPLO 2: WHILE
+
+n = 0
+
+while n<5:
+    print('Número:', n)
+    n += 1
+
+# ATIVIDADE 1: FOR
+
+for i in range(1, 6):
+    print(i**3)
+
+# ATIVIDADE 2: WHILE
+
+numero = 1
+while numero < 100:
+ print(numero)
+ numero += 10
