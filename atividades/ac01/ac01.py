5b57fe339557095ee2cd164616b88d1265f5101d
Letacio-Bezerra <letaciobezerra@gmail.com>
2025-05-27 13:40:44 -0300

    s

--- atividades/ac01/ac01.py
+++ atividades/ac01/ac01.py
@@ -1,4 +1,18 @@
-gastos = input('Qual seus gastos:');
-ganhos = input('Qual seus ganhos:');
+gastos = float(input('Qual seus gastos:'))
+ganhos = float(input('Qual seus ganhos:'))
 
-print('Seu gasto é: ' + gastos + ' e seu ganho é: ' + ganhos)
+saldo = ganhos - gastos
+saldoNegativo = gastos - ganhos
+
+if ganhos == gastos:
+    print('Você está não está no vermelho, mas não pode gastar com mais nada. Pois você tem: ' + str(saldo) + ' real(reais)')
+elif ganhos > gastos:
+        print('Você está positivo, pode gastar até: ' + str(saldo) + ' real(reais)')
+elif ganhos < gastos:
+        print('Você está no negativo, você deve conseguir pelo menos ' + str(saldoNegativo) + ' para sair do vermelho')
+
+while ganhos < gastos:
+    print('Seu saldo está negativo adicione mais dinheiro na sua conta')
+    ganhosExtras = float(input('Adicionar:'))
+    ganhos += ganhosExtras
+    print('Você saiu do vermelho, seu saldo atual é:' + str(ganhos - gastos))
