a587e345f3f303c54f90dbc432616ea763d00496
Letacio-Bezerra <letaciobezerra@gmail.com>
2025-05-29 12:39:57 -0300

    s

--- atividades/ac01/ac01.py
+++ atividades/ac01/ac01.py
@@ -1,51 +1,58 @@
-gastos = float(input('Qual seus gastos:'))
-ganhos = float(input('Qual seus ganhos:'))
+def calcular_saldo(ganhos, gastos):
+    return ganhos - gastos
 
-saldo = ganhos - gastos
-saldoNegativo = gastos - ganhos
+def adicionar_dinheiro(ganhos):
+    adicionar = float(input('Adicionar: '))
+    if adicionar >= 0:
+        ganhos += adicionar
+        print('Você adicionou dinheiro na sua conta, seu saldo atual é: R$' + str(calcular_saldo(ganhos, gastos)))
+    else:
+        print('Valor inválido! Você não pode adicionar valores negativos.')
+    return ganhos
+
+def remover_dinheiro(gastos):
+    remover = float(input('Remover: '))
+    if remover > 0:
+        gastos += remover
+        print('Você removeu dinheiro da sua conta, seu saldo atual é: R$' + str(calcular_saldo(ganhos, gastos)))
+    else:
+        print('Valor inválido! Você deve inserir um valor positivo para remover.')
+    return gastos
+
+gastos = float(input('Qual seus gastos: '))
+ganhos = float(input('Qual seus ganhos: '))
+
+saldo = calcular_saldo(ganhos, gastos)
 
 if ganhos == gastos:
-    print('Você está não está no vermelho, mas não pode gastar com mais nada. Pois você tem: R$' + str(saldo))
+    print('Você não está no vermelho, mas não pode gastar com mais nada. Seu saldo: R$' + str(saldo))
 elif ganhos > gastos:
-        print('Você está positivo, pode gastar até: R$' + str(saldo))
-elif ganhos < gastos:
-        print('Você está no negativo, você deve conseguir pelo menos R$' + str(saldoNegativo) + ' para sair do vermelho')
+    print('Você está positivo, pode gastar até: R$' + str(saldo))
+else:
+    print('Você está no negativo, precisa de pelo menos R$' + str(abs(saldo)) + ' para sair do vermelho')
 
 while ganhos < gastos:
-    print('Seu saldo está negativo adicione mais dinheiro na sua conta')
-    ganhosExtras = float(input('Adicionar:'))
+    print('Seu saldo está negativo, adicione mais dinheiro.')
+    ganhosExtras = float(input('Adicionar: '))
 
     if ganhosExtras > 0:
         ganhos += ganhosExtras
-        print('Você saiu do vermelho, seu saldo atual é: R$' + str(ganhos - gastos))
+        print('Você saiu do vermelho, seu saldo atual é: R$' + str(calcular_saldo(ganhos, gastos)))
     else:
         print('Você não pode tirar mais dinheiro da sua conta')
 
 while True:
     print('Gostaria de adicionar mais dinheiro?')
-    respostaGanhos = input('Sim ou Não?').upper()
+    respostaGanhos = input('Sim ou Não? ').upper()
 
     if respostaGanhos == 'SIM':
-        adicionar = float(input('Adicionar:'))
-        if adicionar >= 0:
-            ganhos += adicionar
-
-            saldo = ganhos - gastos
-            print('Você adicionou dinheiro na sua conta, seu saldo atual é: R$' + str(saldo))
-        else:
-            print('Valor inválido! Você não pode adicionar valores negativos.')
+        ganhos = adicionar_dinheiro(ganhos)
     elif respostaGanhos == 'NÃO':
         print('Gostaria de remover dinheiro da sua conta?')
-        respostaGastos = input('Sim ou Não?').upper()
+        respostaGastos = input('Sim ou Não? ').upper()
 
         if respostaGastos == 'SIM':
-            remover = float(input('Remover:'))
-            if remover > 0:
-                gastos += remover
-                saldo = ganhos - gastos
-                print('Você removeu dinheiro da sua conta, seu saldo atual é: R$' + str(saldo))
-            else:
-                print('Valor inválido! Você deve inserir um valor positivo para remover.')
+            gastos = remover_dinheiro(gastos)
         elif respostaGastos == 'NÃO':
             print('TOP! d(✪ ω ✪)b')
             print('        d b')
