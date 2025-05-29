a4ef13eb23038517a9e6759db35e77cb9e2fa309
Letacio-Bezerra <letaciobezerra@gmail.com>
2025-05-29 09:44:22 -0300

    save

--- atividades/ac01/ac01.py
+++ atividades/ac01/ac01.py
@@ -5,14 +5,32 @@ saldo = ganhos - gastos
 saldoNegativo = gastos - ganhos
 
 if ganhos == gastos:
-    print('Você está não está no vermelho, mas não pode gastar com mais nada. Pois você tem: ' + str(saldo) + ' real(reais)')
+    print('Você está não está no vermelho, mas não pode gastar com mais nada. Pois você tem: R$' + str(saldo))
 elif ganhos > gastos:
-        print('Você está positivo, pode gastar até: ' + str(saldo) + ' real(reais)')
+        print('Você está positivo, pode gastar até: R$' + str(saldo))
 elif ganhos < gastos:
-        print('Você está no negativo, você deve conseguir pelo menos ' + str(saldoNegativo) + ' para sair do vermelho')
+        print('Você está no negativo, você deve conseguir pelo menos R$' + str(saldoNegativo) + ' para sair do vermelho')
 
 while ganhos < gastos:
     print('Seu saldo está negativo adicione mais dinheiro na sua conta')
     ganhosExtras = float(input('Adicionar:'))
-    ganhos += ganhosExtras
-    print('Você saiu do vermelho, seu saldo atual é:' + str(ganhos - gastos))
+
+    if ganhosExtras > 0:
+        ganhos += ganhosExtras
+        print('Você saiu do vermelho, seu saldo atual é: R$' + str(ganhos - gastos))
+    else:
+        print('Você não pode tirar mais dinheiro da sua conta')
+
+while ganhos > gastos:
+    print('Seu saldo está positivo, gostaria de adicionar mais dinheiro?')
+    respostaGanhos = input('Sim ou Não?').upper()
+
+    if respostaGanhos == 'SIM':
+        adicionar = float(input('Adicionar:'))
+        if adicionar > 0:
+            ganhos += adicionar
+            print('Você adicionou dinheiro na sua conta, seu saldo atual é: R$' + str(ganhos))
+        else:
+            print('Você tirou dinheiro da sua conta, seu saldo atual é: R$' + str(ganhos - adicionar))
+    else:
+        print('Ok')
